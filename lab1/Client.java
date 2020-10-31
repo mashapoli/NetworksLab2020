26c8ea667fecc35494b187d5e89b0a8cc50b14a1
mashapoli <mashapolichuk911@gmail.com>
2020-10-31 05:51:11 +0300

    first lab

---
+++ lab1/Client.java
@@ -0,0 +1,130 @@
+import java.net.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+class ClientSomthing {
+
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private BufferedReader inputUser;
+    private final String addr;
+    private final int port;
+    private String nickname;
+
+
+    public ClientSomthing(String addr, int port) {
+        this.addr = addr;
+        this.port = port;
+        try {
+            this.socket = new Socket(addr, port);
+        } catch (IOException e) {
+            System.err.println("Socket failed");
+        }
+        try {
+            inputUser = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            nickname = pressNickname();
+            new ReadMsg().start();
+            new WriteMsg().start();
+        } catch (IOException e) {
+            ClientSomthing.this.downService();
+        }
+    }
+
+
+    private String pressNickname() {
+        System.out.print("Press your nick: ");
+        try {
+            return inputUser.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void downService() {
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+            }
+            System.exit(0);
+        } catch (IOException ignored) {}
+    }
+
+    private class ReadMsg extends Thread {
+        @Override
+        public void run() {
+
+            String str;
+            try {
+                while (true) {
+                    str = in.readLine();
+                    if(str.equals(null)) {
+                        ClientSomthing.this.downService();
+                        break;
+                    }
+                    if (str.equals("stop")) {
+                        ClientSomthing.this.downService();
+                        break;
+                    }
+
+                    String millisStr = str.substring( str.indexOf("<") + 1, str.indexOf("> "));
+                    long millisLong = Long.parseLong(millisStr);
+                    Date time = new Date(millisLong);
+                    SimpleDateFormat dt1 = new SimpleDateFormat("HH:mm:ss.SSS");
+                    String dtime = dt1.format(time);
+
+                    System.out.println("<" + dtime + ">" + str.substring(str.indexOf("> ") +1));
+                }
+            } catch (IOException e) {
+                System.out.println("Sorry, connection has been closed");
+            } finally {
+                System.out.println("Sorry, check connect");
+                ClientSomthing.this.downService();
+            }
+
+        }
+    }
+
+    public class WriteMsg extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String userWord;
+                    userWord = inputUser.readLine();
+                    long currentTime = System.currentTimeMillis();
+                    if (userWord.equals("stop")) {
+                        out.write("stop" + "\n");
+                        ClientSomthing.this.downService();
+                        break;
+                    } else {
+                        out.write("<" + currentTime + "> " + "[" + nickname + "]\0: " + userWord + "\0\n");
+                    }
+                    out.flush();
+                } catch (IOException e) {
+                    ClientSomthing.this.downService();
+
+                }
+
+            }
+        }
+    }
+}
+
+public class Client {
+
+    public static String ipAddr = "localhost";
+    public static int port = 8080;
+
+
+    public static void main(String[] args) {
+        new ClientSomthing(ipAddr, port);
+    }
+}
---
+++ lab1/Server.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+
+class ServerSomthing extends Thread {
+
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+
+
+    public ServerSomthing(Socket socket) throws IOException {
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        start();
+    }
+    @Override
+    public void run() {
+        String word;
+        try {
+            try {
+                while (true) {
+                    word = in.readLine();
+                    if(word.equals("stop")) {
+                        this.downService();
+                        break;
+                    }
+                    String millisStr = word.substring(word.indexOf("<") + 1, word.indexOf("> "));
+                    long millisLong = Long.parseLong(millisStr);
+                    Date time = new Date(millisLong);
+                    SimpleDateFormat dt1 = new SimpleDateFormat("HH:mm:ss.SSS");
+                    String dtime = dt1.format(time);
+                    System.out.println("Echoing: " + "<" + dtime + ">" +  word.substring(word.indexOf("> ") +1));
+                    Thread.sleep(5000);
+                    for (ServerSomthing vr : Server.serverList) {
+                        vr.send(word);
+                    }
+                }
+            } catch (NullPointerException ignored) {} catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (IOException e) {
+            this.downService();
+        }
+    }
+
+
+    private void send(String msg) {
+        try {
+            out.write(msg + "\n");
+            out.flush();
+        } catch (IOException ignored) {}
+
+    }
+
+
+    private void downService() {
+        try {
+            if(!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+                for (ServerSomthing vr : Server.serverList) {
+                    if(vr.equals(this)) vr.interrupt();
+                    Server.serverList.remove(this);
+                }
+            }
+        } catch (IOException ignored) {}
+    }
+}
+
+
+public class Server {
+
+    public static final int PORT = 8080;
+    public static LinkedList<ServerSomthing> serverList = new LinkedList<>();
+
+
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(PORT);
+        System.out.println("Server Started");
+        try {
+            while (true) {
+                Socket socket = server.accept();
+                try {
+                    serverList.add(new ServerSomthing(socket));
+                } catch (IOException e) {
+                    socket.close();
+                }
+            }
+        } finally {
+            server.close();
+        }
+    }
+}
